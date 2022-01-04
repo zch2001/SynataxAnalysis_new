5bc1477c48208bca978f8fc4ed5379fede224e23
zch2001 <59161584+zch2001@users.noreply.github.com>
2022-01-04 22:47:08 +0800

    Add files via upload

---
+++ SynataxAnalysis.cpp
@@ -0,0 +1,396 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <stdexcept>
+
+class SynAna
+{
+public:
+    SynAna() = default;
+    void analyse(std::string in) {
+        try {
+            analyseFun(in);
+        }
+        catch (std::exception& e) {
+            std::cerr << "! EXCEPTION: " << e.what() << std::endl;
+            return ;
+        }
+    }
+
+private:
+    std::stack<int>state;
+    std::stack<std::string>symbol;
+    std::stack<std::string>w;
+    std::string action[30][8] = {
+   /* (      )     +     -     *     /    num    $   */
+    {"S4" , ""  , ""  , ""  , ""  , ""  ,"S5" , ""  },             //0
+    { ""  , ""  ,"S6" ,"S7" , ""  , ""  , ""  ,"ACC"},          //1
+    { ""  , ""  ,"R3" ,"R3" ,"S8" ,"S9" , ""  ,"R3" },       //2
+    { ""  , ""  ,"R6" ,"R6" ,"R6" ,"R6" , ""  ,"R6" },       //3
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //4
+    { ""  , ""  ,"R8" ,"R8" ,"R8" ,"R8" , ""  ,"R8" },       //5
+    {"S4" , ""  , ""  , ""  , ""  , ""  ,"S5" , ""  },             //6
+    {"S4" , ""  , ""  , ""  , ""  , ""  ,"S5" , ""  },             //7
+    {"S4" , ""  , ""  , ""  , ""  , ""  ,"S5" , ""  },             //8
+    {"S4" , ""  , ""  , ""  , ""  , ""  ,"S5" , ""  },             //9
+    { ""  ,"S19","S20","S21", ""  , ""  , ""  , ""  },        //10
+    { ""  ,"R3" ,"R3" ,"R3" ,"S22","S23", ""  , ""  },     //11
+    { ""  ,"R6" ,"R6" ,"R6" ,"R6" ,"R6" , ""  , ""  },       //12
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //13
+    { ""  ,"R8" ,"R8" ,"R8" ,"R8" ,"R8" , ""  , ""  },       //14
+    { ""  , ""  ,"R1" ,"R1" ,"S8" ,"S9" , ""  ,"R1" },       //15
+    { ""  , ""  ,"R2" ,"R2" ,"S8" ,"S9" , ""  ,"R2" },       //16
+    { ""  , ""  ,"R4" ,"R4" ,"R4" ,"R4" , ""  ,"R4" },       //17
+    { ""  , ""  ,"R5" ,"R5" ,"R5" ,"R5" , ""  ,"R5" },       //18
+    { ""  , ""  ,"R7" ,"R7" ,"R7" ,"R7" , "" , "R7" },       //19
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //20
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //21
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //22
+    {"S13", ""  , ""  , ""  , ""  , ""  ,"S14", ""  },           //23
+    { ""  ,"S28","S20","S21", ""  , ""  , ""  , ""  },        //24
+    { ""  ,"R1" ,"R1" ,"R1" ,"S22","S23", ""  , ""  },     //25
+    { ""  ,"R4" ,"R4" ,"R4" ,"R4" ,"R4" , ""  , ""  },       //26
+    { ""  ,"R5" ,"R5" ,"R5" ,"R5" ,"R5" , ""  , ""  },       //27
+    { ""  ,"R7" ,"R7" ,"R7" ,"R7" ,"R7" , ""  , ""  },       //28
+    { ""  ,"R2" ,"R2" ,"R2" ,"S22","S23", ""  , ""  } };   //29
+    float synaGoto[30][3] = {
+    {1,2,3},            //0
+    {NAN,NAN,NAN},      //1
+    {NAN,NAN,NAN},      //2
+    {NAN,NAN,NAN},      //3
+    {10,11,12},         //4
+    {NAN,NAN,NAN},      //5
+    {NAN,15,3},         //6
+    {NAN,16,3},         //7
+    {NAN,NAN,17},       //8
+    {NAN,NAN,18},       //9
+    {NAN,NAN,NAN},      //10
+    {NAN,NAN,NAN},      //11
+    {NAN,NAN,NAN},      //12
+    {24,11,12},         //13
+    {NAN,NAN,NAN},      //14
+    {NAN,NAN,NAN},      //15
+    {NAN,NAN,NAN},      //16
+    {NAN,NAN,NAN},      //17
+    {NAN,NAN,NAN},      //18
+    {NAN,NAN,NAN},      //19
+    {NAN,25,12},        //20
+    {NAN,29,12},        //21
+    {NAN,NAN,26},       //22
+    {NAN,NAN,27},       //23
+    {NAN,NAN,NAN},      //24
+    {NAN,NAN,NAN},      //25
+    {NAN,NAN,NAN},      //26
+    {NAN,NAN,NAN},      //27
+    {NAN,NAN,NAN},      //28
+    {NAN,NAN,NAN}       //29
+    };
+    const char* expre[9] = { nullptr,"E->E+T","E->E-T","E->T","T->T*F","T->T/F","T->F","F->(E)","F->num" };
+    void reduce(std::string actionString) {
+        switch (std::atoi(actionString.c_str() + 1)) {
+        case 1: //用产生式E->E+T
+            for (int i = 0; i < 3; i++) {
+                state.pop();
+                symbol.pop();
+            }
+            //将当前元素push进去
+            symbol.push("E");
+            if (synaGoto[state.top()][0] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][0]);
+            break;
+        case 2: //用产生式E->E-T
+            for (int i = 0; i < 3; i++) {
+                state.pop();
+                symbol.pop();
+            }
+            symbol.push("E");
+            if (synaGoto[state.top()][0] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][0]);
+            break;
+        case 3://用产生式E->T
+            state.pop();
+            symbol.pop();
+            symbol.push("E");
+            if (synaGoto[state.top()][0] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][0]);
+            break;
+        case 4:  //用产生式T->T*F
+            for (int i = 0; i < 3; i++) {
+                state.pop();
+                symbol.pop();
+            }
+            //将当前元素push进去
+            symbol.push("T");
+            if (synaGoto[state.top()][1] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][1]);
+            break;
+        case 5: //用产生式T->T/F
+            for (int i = 0; i < 3; i++) {
+                state.pop();
+                symbol.pop();
+            }
+            //将当前元素push进去
+            symbol.push("T");
+            if (synaGoto[state.top()][1] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][1]);
+            break;
+        case 6: //用产生式T->F
+            state.pop();
+            symbol.pop();
+            symbol.push("T");
+            if (synaGoto[state.top()][1] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][1]);
+            break;
+        case 7: //用产生式F->(E)
+            //pop出3个元素
+            for (int i = 0; i < 3; i++) {
+                state.pop();
+                symbol.pop();
+            }
+            symbol.push("F");
+            if (synaGoto[state.top()][2] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][2]);
+            break;
+        case 8: //用产生式F->num
+            state.pop();
+            symbol.pop();
+            symbol.push("F");
+            if (synaGoto[state.top()][2] == NAN) throw std::runtime_error("gotoException");
+            state.push(synaGoto[state.top()][2]);
+            break;
+        }
+    }
+    template <typename T>
+    std::string stack_to_str(std::stack<T> st) { //输出栈 转换为字符串
+        std::stringstream ss;
+        ss << '[';
+        std::stack<T> stack;
+        while (!st.empty()) {
+            stack.push(st.top());
+            st.pop();
+        }
+        for (; !stack.empty(); stack.pop()) {
+            T element = stack.top();
+            ss << element;
+            ss << ", ";
+        }
+        std::string s = ss.str().substr(0, ss.str().length() - 2); //删除最后一个", "
+        s += ']';
+        return s;
+    }
+    void output(int step, std::string action_string) {
+        // 输出步骤序号
+        std::cout << step << "\t";
+        // 输出状态栈
+        std::printf("%-30s", stack_to_str(state).c_str());
+        // 输出符号栈
+        std::printf("%-26s\t", stack_to_str(symbol).c_str());
+        // 输出输入符号流
+        std::string expressionString;
+        for (auto dump = w; !dump.empty(); dump.pop())
+            expressionString += dump.top();
+        std::printf("%25s", (expressionString).c_str());
+        // 输出分析动作
+        switch (action_string[0]) {
+        case 'R':
+            std::cout << "\treduce " << expre[std::atoi(action_string.c_str() + 1)] << std::endl;
+            break;
+        case 'S':
+            std::cout << "\tshift " << action_string.c_str() + 1 << std::endl;
+            break;
+        case 'A':
+            std::cout << "\tACC" << std::endl;
+            break;
+        }
+    }
+    void analyseFun(std::string input) {
+        input += "$";
+        std::cout << "输入为：" << input << "\n分析结果：\n";
+        std::stack<std::string> s;
+        for (int i = 0; i < input.length(); i++) {
+            if (input[i] != 'n') s.push(std::string{ input[i] });
+            else {
+                char temp[4] = { 0 };
+                temp[0] = input[i];
+                temp[1] = input[i + 1];
+                temp[2] = input[i + 2];
+                s.push(temp);
+                i = i + 2;
+            }
+        }
+        std::cout << "步骤\t状态栈\t\t\t      符号栈\t\t\t\t             \t输入符号流\t分析动作\n";
+        while (!s.empty()) {
+            w.push(s.top());
+            s.pop();
+        }
+        state.push(0);
+        symbol.push("$");
+        int step = 0;
+        while (!w.empty()) {
+            //栈顶状态
+            step++;
+            int temp_state = state.top();
+            std::string actionString;
+            std::string temp_s = w.top();
+            if (w.top() == "(") {
+                //遇到“(”时的动作
+                actionString = action[temp_state][0];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == ")") {
+                //遇到“)”时的动作
+                actionString = action[temp_state][1];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == "+") {
+                //遇到“+”时的动作
+                actionString = action[temp_state][2];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == "-") {
+                //遇到“-”时的动作
+                actionString = action[temp_state][3];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == "*") {
+                actionString = action[temp_state][4];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == "/") {
+                actionString = action[temp_state][5];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == std::string("num")) {
+                //遇到“num”时的动作
+                actionString = action[temp_state][6];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else if (w.top() == std::string("$")) {
+                //遇到“$”时的动作
+                actionString = action[temp_state][7];
+                if (actionString == std::string(""))
+                    throw std::runtime_error("reduceException ->" + w.top());
+                //接受符号串
+                if (actionString == "ACC") {
+                    output(step, actionString);
+                    state.pop();
+                    symbol.pop();
+                    w.pop();
+                }
+                //移进
+                if (actionString[0] == 'S') {
+                    output(step, actionString);
+                    state.push(std::atoi(actionString.c_str() + 1));
+                    symbol.push(w.top());
+                    w.pop();
+                }
+                //规约
+                else if (actionString[0] == 'R') {
+                    output(step, actionString);
+                    reduce(actionString);
+                }
+            }
+            else throw std::runtime_error("reduceException ->" + w.top());
+        }
+    }
+};
+
+int main() {
+    std::string syna;
+    std::cin >> syna;
+    SynAna synatax = SynAna();
+    synatax.analyse(syna);
+    return 0;
+}
---
+++ myLexiaclAnalysis.cpp
@@ -0,0 +1,256 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <filesystem>
+
+std::string keyword[32] ={ "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
+    "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed",
+    "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while" };//关键字
+char separater[8] = { ';',',','{','}','[',']','(',')' };    //分隔符
+char operate[10] = { '+','-','*','/','>','<','=','!','%','"'};     //运算符
+char interval[4] = { ' ','\t','\r','\n' };
+char wrong[4] = {'“','”','‘','’'};//错误集
+std::string showTimes[6] = { "error","operator","separater","keyword","number","IDENTIFIER"};
+
+class LexiaclAnalysis {
+private:
+    std::string inputBuffer;//存储输入
+    std::vector<std::string>woldList;//存储单词表
+    std::string codeString;
+    int num = 0;
+    int eT, oT, sT, kT, nT, iT = 0;
+    int line=1;
+    FILE* code=NULL;
+    int errorNum = 0;
+    std::string *errorInfo;
+    void checkShowTimes() {
+        for (unsigned i = 2; i < woldList.size();i++ ) {
+            if (woldList[i] == showTimes[0]) {
+                eT++;
+            }
+            if (woldList[i] == showTimes[1]) {
+                oT++;
+            }
+            if (woldList[i] == showTimes[2]) {
+                sT++;
+            }
+            if (woldList[i] == showTimes[3]) {
+                kT++;
+            }
+            if (woldList[i] == showTimes[4]) {
+                nT++;
+            }
+            if (woldList[i] == showTimes[5]) {
+                iT++;
+            }
+        }
+            std::cout << "there is " << eT << " error   " << oT << " operator   "<< sT << " separater   " << kT
+                << " keyword   " << nT << " number   " << iT << " IDENTIFIER" << std::endl;
+    }
+    bool isKeyword(std::string word) {
+        for (int i = 0; i < 32; i++) {
+            if (keyword[i] == word)
+                return true;
+        }
+        return false;
+    }
+    bool isSeparater(char input) {
+        for (int i = 0; i < 8; i++) {
+            if (separater[i] == input)
+                return true;
+        }
+        return false;
+    }
+    bool isWrong(char input) {
+        for (int i = 0; i < 2; i++) {
+            if (wrong[i]==input)
+                return true;
+        }
+        return false;
+    }
+    bool isOperator(char input) {
+        for (int i = 0; i < 10; i++) {
+            if (operate[i] == input)
+                return true;
+        }
+        return false;
+    }
+    bool isInterval(char input) {
+        if (input=='\n') {
+            line++;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (interval[i] == input)
+                return true;
+        }
+        return false;
+    }
+    bool isLetter(char input) {
+        if ((input >= 'a' && input <= 'z')|| (input >= 'A' && input <= 'Z'))
+            return true;
+        return false;
+    }
+    bool isDigit(char input) {
+        if (input >= '0' && input <= '9')
+            return true;
+        return false;
+    }
+    template <class T>
+    int value(T* a, int n, T str) {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == str)
+                return i + 1;
+        }
+        return -1;
+    }
+    void destoryInputBUffer() {
+        inputBuffer.erase(inputBuffer.begin(), inputBuffer.end());
+    }
+    void freeWoldLsit() {
+        woldList.clear();
+    }
+    void LexScan() {
+        std::string temp = "";
+        inputBuffer = "";
+        std::cout << codeString << std::endl;
+        for (int i = 0; i < codeString.size(); i++) {
+            if (codeString[i]=='/'&&codeString[i+1]=='/') {
+                while (codeString[i] != '\n')
+                    i++;
+            }
+            if (codeString[i] == '/' && codeString[i+1] == '*') {
+                i += 2;
+                while (codeString[i] != '*' && codeString[i + 1] != '/') {
+                    i++;
+                }
+                if (codeString[i] == '*' && codeString[i + 1] == '/') {
+                    i++;
+                }
+            }
+            else {
+            if (isInterval(codeString[i]))
+                continue;
+                if (isOperator(codeString[i])) {
+                    woldList.push_back(std::to_string(value(operate, (sizeof(operate) / sizeof(operate[0])), codeString[i])));
+                    temp += codeString[i];
+                    woldList.push_back(temp);
+                    woldList.push_back("operator");
+                    num++;
+                    temp = "";
+                    continue;
+                }
+                if (isSeparater(codeString[i])) {
+                    woldList.push_back(std::to_string(value(separater, (sizeof(separater) / sizeof(separater[0])), codeString[i])));
+                    temp += codeString[i];
+                    woldList.push_back(temp);
+                    woldList.push_back("separater");
+                    num++;
+                    temp = "";
+                    continue;
+                }
+                if (isWrong(codeString[i])) {
+                    woldList.push_back("-1");
+                    temp += codeString[i];
+                    woldList.push_back(temp);
+                    woldList.push_back("error");
+                    errorNum++;
+                    *(errorInfo + errorNum - 1) = "error " + std::to_string(errorNum) + " in line " + std::to_string(line);
+                    temp = "";
+                    goto err;
+                }
+                if (isDigit(codeString[i])) {
+                    temp += codeString[i];
+                    while (isDigit(codeString[i + 1])) {
+                        i++;
+                        temp += codeString[i];
+                        if (isLetter(codeString[i+1])) {
+                            while (isLetter(codeString[i+1])){
+                                i++;
+                                temp += codeString[i];
+                            }
+                            woldList.push_back("-1");
+                            woldList.push_back(temp);
+                            woldList.push_back("error");
+                            errorNum++;
+                            *(errorInfo + errorNum - 1) = "error " + std::to_string(errorNum) + " in line " + std::to_string(line);
+                            temp = "";
+                            goto err;
+                        }
+                    }
+                    woldList.push_back("101");
+                    woldList.push_back(temp);
+                    woldList.push_back("number");
+                    temp = "";
+                    continue;
+                }
+                err:
+                if (isLetter(codeString[i])) {
+                    temp += codeString[i];
+                    while (isDigit(codeString[i + 1]) || isLetter(codeString[i + 1])) {
+                        i++;
+                        temp += codeString[i];
+                    }
+                    if (isKeyword(temp)) {
+                        woldList.push_back(std::to_string(value(keyword, (sizeof(keyword) / sizeof(keyword[0])), temp)));
+                        woldList.push_back(temp);
+                        woldList.push_back("keyword");
+                        temp = "";
+                        continue;
+                    }
+                    else {
+                        woldList.push_back("100");
+                        woldList.push_back(temp);
+                        woldList.push_back("IDENTIFIER");
+                        temp = "";
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+public:
+    LexiaclAnalysis() {
+        fopen_s(&code,"./resorse.txt", "r");
+        char c;
+        while ((c = getc(code)) != EOF){
+            codeString += c;
+        }
+        errorInfo = new std::string[100];
+    }
+    LexiaclAnalysis(char *address) {
+        fopen_s(&code, address, "r");
+        char c;
+        while ((c = getc(code)) != EOF) {
+            codeString += c;
+        }
+        errorInfo = new std::string[100];
+    }
+    void LexPrint() {
+        LexScan();
+        std::cout<<"值\t"<<"单词\t"<<"记号\t"<<std::endl;
+        for (unsigned i = 0; i < woldList.size(); ){
+        std::cout << woldList[i] << "\t" << woldList[i + 1] << "\t" << woldList[i + 2] << std::endl;
+        i += 3;
+        }
+        std::cout << "there is " << line << " line" << std::endl;
+        for (int i = 0; i < errorNum; i++){
+            if (errorInfo[i] != "") {
+
+                std::cout << *(errorInfo+i) << "\n";
+            }
+            else{
+                break;
+            }
+        }
+        checkShowTimes();
+        destoryInputBUffer();
+        freeWoldLsit();
+        delete[]errorInfo;
+    }
+};
+int main(){
+    LexiaclAnalysis lex = LexiaclAnalysis();
+    lex.LexPrint();
+    return 0;
+}
